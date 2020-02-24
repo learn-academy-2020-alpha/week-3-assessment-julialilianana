f7b44739910f0b2fda363e731081c12b5ec8a1f6
Julia <jlilianacollins@gmail.com>
2020-02-24 08:48:15 -0800

    finished interview questions and react comment

--- code-challenges.js
+++ code-challenges.js
@@ -3,8 +3,19 @@
 
 // --------------------1) Create a function that returns the first 10 numbers of the Fibonacci sequence in an array. Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-
-
+const Fibonacci = () => {
+    let newArray= [0,1]
+    let a = 0
+    let b = 1
+    for (let i=0; i<9; i++){
+        added = a + b
+        newArray.push(added)
+        a=b
+        b=added 
+    }
+    return newArray
+}
+console.log(Fibonacci())
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
@@ -13,9 +24,13 @@ var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+const onlyOdd = (array) => {
+    let onlyNum = array.filter(value => typeof value === "number" && value % 2 === 1 || value % 2 === -1 )
+    return onlyNum.sort(function(a, b){return a-b})
+}
 
-
-
+console.log(onlyOdd(fullArr1))
+console.log(onlyOdd(fullArr2))
 // --------------------3) Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
 
 var middleLetters1 = "hello"
@@ -23,12 +38,30 @@ var middleLetters1 = "hello"
 var middleLetters2 = "rhinoceros"
 // Expected output: “oc”
 
+const middleLetter = (string) => {
+    let newArray= string.split("")
+    if(newArray.length % 2 === 1) {
+        middle = Math.ceil(newArray.length/2)
+        return newArray[middle]
+    }if(newArray.length % 2 === 0) {
+        middle = (newArray.length/2)
+        let  middleValue1 = newArray[middle-1]
+        let middleValue2 = newArray[middle]
+        return middleValue1.concat(middleValue2)    
+    }
+}
 
-
+console.log(middleLetter(middleLetters1))
+console.log(middleLetter(middleLetters2))
 
 // --------------------4) Create a class to get the area of a sphere. Create three spheres with different radi as test cases. Area of a sphere =  4πr^2 (four pi r squared)
 
-
+const areaSphere = (radius) => {
+    return (4 * Math.PI * radius * radius)
+}
+console.log(areaSphere(2))
+console.log(areaSphere(5))
+console.log(areaSphere(11))
 
 
 // --------------------5) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
@@ -39,3 +72,22 @@ var numbersToAdd2 = [0, 7, -8, 12]
 // Expected output: [0, 7, -1, 11]
 var numbersToAdd3 = []
 // Expected output: []
+
+const accumulatingSum = (array) => {
+        let newArray= []
+        
+        for (i=0; i<array.length; i++){
+            let i=0
+        let x = array[i]
+        let y = array[i+1]
+            added = x + y
+            newArray.push(added)
+            x=added
+            
+
+        }
+        return newArray
+}
+console.log(accumulatingSum(numbersToAdd1))
+console.log(accumulatingSum(numbersToAdd2))
+console.log(accumulatingSum(numbersToAdd3))
--- react-commenting.js
+++ react-commenting.js
@@ -7,19 +7,20 @@
 class App extends Component{
   constructor(){
     super()
-    // 1.
+    // 1.      userInput is an empty string to be updated to what the user types into the input box
+
     this.state = {
       userInput: "",
     }
   }
 
   robot = (e) => {
-    // 2.
+    // 2. robot function that updates the userinput to be whatever is typed into the input box
     this.setState({ userInput: e.target.value})
   }
 
   render(){
-    // 3.
+    // 3. destructuring userInput
     let { userInput } = this.state
 
     return(
@@ -28,16 +29,16 @@ class App extends Component{
 
         <h3>Enter text here:</h3>
         <input
-          {/* 4. */}
+          {/* 4. onChange the funtion robot is called to update the text */}
           onChange={ this.robot }
-          {/* 5. */}
+          {/* 5. the vaue of the text is the updated userInput, or whatever the user has typed into the input box*/}
           value={ userInput }
         />
 
         <div>
-          {/* 6. */}
+          {/* 6. GoodRobot component */}
           <GoodRobot
-            {/* 7. */}
+            {/* 7. passing the props of the userInput to the good robot */}
             userInput={ userInput }
           />
           <BadRobot
@@ -58,10 +59,10 @@ export default App
 class GoodRobot extends Component{
   render(){
     return(
-      // 8.
+      // 8. inside the good robot component
       <div>
         <h3>Good Robot</h3>
-        {/* 9. */}
+        {/* 9. this is what the good robot says, it includes the props of userInput*/}
         <h4>I hear you saying { this.props.userInput }. Is that correct?</h4>
       </div>
     )
